2ce5eb68aa4968a5e228a1bfa6f883278521e06e
Joakim Adrup <adrup@live.com>
2017-11-16 16:14:50 +0100

    radar objects with movement

--- Assets/Scripts/MarkerBehaviour.cs
+++ Assets/Scripts/MarkerBehaviour.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MarkerBehaviour : MonoBehaviour {
 
 	public float height;
+	public float speed;
+	public Vector2 direction;
 
 	private GameObject jet;
 	private GameObject line;
@@ -13,15 +15,14 @@ public class MarkerBehaviour : MonoBehaviour {
 	void Start () {
 		jet = this.transform.GetChild (0).gameObject;
 		line = this.transform.GetChild (1).gameObject;
+
+		direction.Normalize ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 pos;
-		pos.x = this.transform.position.x;
-		pos.y = height;
-		pos.z = this.transform.position.z;
-		jet.transform.position = pos;
+		this.transform.position += new Vector3(direction.x * speed * Time.deltaTime, 0, direction.y * speed * Time.deltaTime);
+		jet.transform.position += new Vector3(0, height - jet.transform.localPosition.y, 0);
 		line.transform.localScale = new Vector3 (1.0f, height, 1.0f);
 	}
 }
---
+++ Assets/Scripts/SpawnObjects.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjects : MonoBehaviour {
+
+	[System.Serializable]
+	public struct Marker{
+		public GameObject prefab;
+		public Vector2 direction;
+		public float height;
+		public float speed;
+		public float spawn_time;
+		public Vector2 initial_position;
+	};
+
+	public List<Marker> objects;
+	public float radar_range;
+
+	private List<GameObject> active_objects;
+
+	private GameObject marker_space;
+
+	void Start(){
+		marker_space = new GameObject ("Markers");
+		active_objects = new List<GameObject> ();
+	}
+	
+	// Spawning prefabs
+	void Update () {
+		foreach(Marker m in objects){
+			if (Time.time > m.spawn_time) {
+				GameObject g  = Instantiate (m.prefab, m.initial_position, Quaternion.identity, marker_space.transform);
+				active_objects.Add (g);
+				g.GetComponent<MarkerBehaviour> ().height = m.height;
+				g.GetComponent<MarkerBehaviour> ().direction = m.direction;
+				g.GetComponent<MarkerBehaviour> ().speed = m.speed;
+				objects.Remove (m);
+			}
+		}
+	}
+}
